1ab2cb122a50bbf57bd9481081b9ca7d48f5cbbf
rai-n <neeraj.rai@city.ac.uk>
2023-02-28 11:40:03 +0000

    Update DeleteNode.java

--- Java/Data Structures/Linked List/DeleteNode.java
+++ Java/Data Structures/Linked List/DeleteNode.java
@@ -43,8 +43,7 @@ class SinglyLinkedList {
 }
 
 class SinglyLinkedListPrintHelper {
-    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter)
-            throws IOException {
+    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
         while (node != null) {
             bufferedWriter.write(String.valueOf(node.data));
 
@@ -64,42 +63,46 @@ class Result {
      *
      * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
      * The function accepts following parameters:
-     * 1. INTEGER_SINGLY_LINKED_LIST llist
-     * 2. INTEGER position
+     *  1. INTEGER_SINGLY_LINKED_LIST llist
+     *  2. INTEGER position
      */
 
     /*
      * For your reference:
      *
      * SinglyLinkedListNode {
-     * int data;
-     * SinglyLinkedListNode next;
+     *     int data;
+     *     SinglyLinkedListNode next;
      * }
      *
      */
 
     public static SinglyLinkedListNode deleteNode(SinglyLinkedListNode llist, int position) {
-        // 0 -> 1 -> 2 -> 3, 2
-        // 0 -> 1 -> 3
-        if (llist == null) {
-            return null;
-        }
+    // 0 -> 1 -> 2 -> 3, 2
+    // 0 -> 1 -> 3 
+    if (llist == null) {
+        return null; 
+    } 
+    
+    if (position == 0){
+        return llist.next; 
+    }
+    
+    SinglyLinkedListNode prev = null; 
+    SinglyLinkedListNode curr = llist; 
 
-        if (position == 0) {
-            return llist.next;
-        }
-
-        SinglyLinkedListNode prev = null;
-        SinglyLinkedListNode curr = llist;
-
-        for (int i = 0; i < position; i++) {
-            prev = curr;
-            curr = curr.next;
-        }
-
-        prev.next = curr.next;
-        return llist;
-
+ 
+    for (int i=0; i < position; i++){
+        // To have reference to previous node
+        prev = curr; 
+        curr = curr.next;
+    }
+    // Setting previous's next node reference to the node after the node to delete
+    prev.next = curr.next;
+    
+    // Returning head
+    return llist; 
+    
     }
 
 }
